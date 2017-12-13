741cb59cc59471b47974316f10933e9021538c41
DANIELE-XPS15\danie <d.palbanese@gmail.com>
2017-12-13 17:10:18 +0100

    trying to implement rooms

--- src/main.go
+++ src/main.go
@@ -6,11 +6,19 @@ import (
 
 	"github.com/gorilla/websocket"
 	"fmt"
+	"github.com/mitchellh/mapstructure"
+)
+
+const(
+	MESSAGE_REC = iota 	//0
+	JOINED_ROOM 		//1
+	CREATED_ROOM		//2
 )
 
 var (
-	clients = make(map[*websocket.Conn]string) 	// connected clients
-	broadcast = make(chan Message)				// broadcast channel
+	rooms = make(map[string]*Room) 			//map of rooms
+	broadcast = make(chan Message, 32)		// broadcast channel
+
 	upgrader = websocket.Upgrader{}
 	systemAuthor = Author{Username:"SYSTEM", AvatarUrl:""}
 )
@@ -20,9 +28,20 @@ type Author struct {
 	AvatarUrl string `json:"avatarurl"`
 }
 
+type Room struct {
+	clients  map[*websocket.Conn]string 	// connected clients
+	Name string
+}
+
+type RoomAction struct {
+	Id string `json:"id"`
+	Name string `json:"name"`
+}
+
 type Message struct {
 	Author Author `json:"author"`
 	Message string `json:"message"`
+	RoomId string `json:"roomid"`
 }
 
 func main() {
@@ -50,18 +69,58 @@ func handleConnections(w http.ResponseWriter, r *http.Request){
 	// Make sure we close the connecton when the function returns
 	defer ws.Close()
 	//Get Username of user
-	var author Author
-	err = ws.ReadJSON(&author)
+	var client Author
+	err = ws.ReadJSON(&client)
 	if err != nil {
 		log.Printf("error: %v", err)
 		return
 	}
-	fmt.Println("User Registered: ",author.Username)
-	// Register our new client
-	clients[ws] = author.Username
+	fmt.Println("User Registered: ",client.Username)
+	//Send list of rooms
+	err = ws.WriteJSON(rooms)
+	if err != nil {
+		log.Printf("error: %v", err)
+		return
+	}
+
+	//wait for user to join or create a Room
+	var data map[string]interface{}
+	err = ws.ReadJSON(&data)
+	if err != nil {
+		log.Printf("error: %v", err)
+		return
+	}
+	opC := data["opcode"].(int)
+	//try converting to room action
+	delete(data, "opcode")
+	var ra RoomAction
+	err = mapstructure.Decode(data, &ra)
+	if err != nil {
+		log.Printf("error: %v", err)
+		return
+	}
+	//Check what the user did
+	var room *Room
+	switch opC {
+	case JOINED_ROOM:
+		//Join room
+		var ok bool
+		room, ok = rooms[ra.Id]
+		if !ok {
+			log.Println("COULDN'T FIND ROOM IN LIST")
+			return
+		}
+		// Register our new client
+		room.clients[ws] =client.Username
+	case CREATED_ROOM:
+		//Create room
+	default:
+		log.Println("User tried something before joining room. Close connection")
+		return
+	}
 
 	//Send welcome message
-	welcomeMSG := Message{Author:systemAuthor, Message: author.Username+" joined the chat!"}
+	welcomeMSG := Message{Author:systemAuthor, Message: client.Username+" joined the chat!", RoomId:ra.Id}
 	broadcast <- welcomeMSG
 
 	// Infinite loop to wait and read messages from websocket
@@ -71,9 +130,9 @@ func handleConnections(w http.ResponseWriter, r *http.Request){
 		err = ws.ReadJSON(&msg)
 		if err != nil {
 			log.Printf("error: %v", err)
-			delete(clients, ws)
+			delete(room.clients, ws)
 			//broadcast that user left
-			broadcast <- Message{Author:systemAuthor, Message: author.Username+ " left the chat!"}
+			broadcast <- Message{Author:systemAuthor, Message: client.Username+ " left the chat!", RoomId:ra.Id}
 			break
 		}
 		// Send the newly received message to the broadcast channel
@@ -86,15 +145,15 @@ func handleMessages(){
 		// Grab the next message from the broadcast channel
 		msg := <-broadcast
 		// Send it out to every client that is currently connected
-		for client:= range clients {
+		for client:= range rooms[msg.RoomId].clients {
 			err:= client.WriteJSON(msg)
 			if err != nil {
 				log.Printf("error: %v", err)
-				username:= clients[client]
+				username:= rooms[msg.RoomId].clients[client]
 				client.Close()
-				delete(clients, client)
+				delete(rooms[msg.RoomId].clients, client)
 				//broadcast that user left
-				broadcast <- Message{Author:systemAuthor, Message: username+" left the chat!"}
+				broadcast <- Message{Author:systemAuthor, Message: username+" left the chat!", RoomId: msg.RoomId}
 			}
 		}
 	}
